523a3f5800432c01dcc77a2b2e6b9ae6677d69ef
JoRuPeRu <johan.rutberg@iths.se>
2024-06-07 15:13:14 +0200

    Chart

--- backend/pages/index.js
+++ backend/pages/index.js
@@ -1,11 +1,23 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { IoHome } from "react-icons/io5";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LineController,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  LinearScale
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import Loading from "@/components/Loading";
 
 export default function Home() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   const router = useRouter();
   // check if there is no active session and redirect to login page
@@ -15,6 +27,91 @@ export default function Home() {
       router.push("/login");
     }
   }, [session, router]);
+
+  if (status === "loading") {
+    // loading state, loader or any other indicator
+    return (
+      <div className="loadingdata flex flex-col flex-center wh_100">
+        <Loading />
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  ChartJS.register(CategoryScale, LineController, LinearScale, BarElement, Title, Tooltip, Legend);
+
+  // Use this on top for render error
+  const [blogsData, setBlogsData] = useState([]);
+
+  // Define options within the component
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top"
+      },
+      title: {
+        display: true,
+        text: "Bloggar Skapade Månadsvis Per År"
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Fetch data from api
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/blogapi");
+        const data = await response.json();
+        setBlogsData(data); // Assuming data is an array of the blog oject
+      } catch (error) {
+        console.error("Error fetching data.", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // Aggregate data by year and month
+  const monthlydata = blogsData
+    .filter((dat) => dat.status === "publish")
+    .reduce((acc, blog) => {
+      const year = new Date(blog.createdAt).getFullYear(); // Get the year
+      const month = new Date(blog.createdAt).getMonth(); // Get the month (0 indexed)
+      acc[year] = acc[year] || Array(12).fill(0); // Initialize array for the year if not exists
+
+      acc[year][month]++; // Increment count for the month
+      return acc;
+    }, {});
+
+  const currentYear = new Date().getFullYear(); // Get the current year
+  const years = Object.keys(monthlydata);
+  const labels = [
+    "Januari",
+    "Februari",
+    "Mars",
+    "April",
+    "Maj",
+    "Juni",
+    "Juli",
+    "Augusti",
+    "September",
+    "Oktober",
+    "November",
+    "December"
+  ];
+
+  const datasets = years.map((year) => ({
+    label: `${year}`,
+    data: monthlydata[year] || Array(12).fill(0), // If no data for a month, default to 0
+    backgroundColor: `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, 0.5)`
+  }));
+
+  const data = {
+    labels,
+    datasets
+  };
 
   if (session) {
     return (
@@ -44,7 +141,7 @@ export default function Home() {
           <div className="topfourcards flex flex-sb">
             <div className="four_card">
               <h2>Total Blogs</h2>
-              <span>10</span>
+              <span>{blogsData.filter((ab) => ab.status === "publish").length}</span>
             </div>
             <div className="four_card">
               <h2>Total Topics</h2>
@@ -56,7 +153,7 @@ export default function Home() {
             </div>
             <div className="four_card">
               <h2>Draft Blogs</h2>
-              <span>10</span>
+              <span>{blogsData.filter((ab) => ab.status === "draft").length}</span>
             </div>
           </div>
           {/* year overview */}
@@ -76,7 +173,7 @@ export default function Home() {
                   10 / 365 <br /> <span>Total Published</span>
                 </h3>
               </div>
-              {/* Chart... 1:23:45 */}
+              <Bar data={data} options={options} />
             </div>
             <div className="right_salescont">
               <div>
