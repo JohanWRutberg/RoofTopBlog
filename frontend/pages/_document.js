0e5d0174d40552d2c654aa9bc509289a18b79843
JoRuPeRu <johan.rutberg@iths.se>
2024-08-12 13:02:24 +0200

    Google Analytics update

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,13 +5,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <scrip async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-T26PQD5S');`
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,});
+            `
           }}
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -23,14 +26,14 @@ export default function Document() {
       </Head>
 
       <body>
-        <noscript>
+        {/* <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-T26PQD5S"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
-        </noscript>
+        </noscript> */}
         <Main />
         <NextScript />
       </body>
