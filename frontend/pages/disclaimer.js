a3e5954c7477987f72f265fe7e23fd54dc09b284
JoRuPeRu <johan.rutberg@iths.se>
2024-08-19 00:42:43 +0200

    Disclaimer updated

--- frontend/pages/disclaimer.js
+++ frontend/pages/disclaimer.js
@@ -12,33 +12,17 @@ export default function DisclaimerPage() {
                 <h2>How do we make money from our blog?</h2>
                 Some links on our blog are "affiliate links," which contain a special tracking code. <br />
                 This means if you click on an affiliate link and buy something, we get a commission at no extra cost to
-                you. <br />
-                <br />
-                The price is the same whether you use an affiliate link or not.
-                <h2>FTC disclosure compliance rules</h2>
-                In 2015, the Federal Trade Commission set rules for Disclosure Compliance.
-                <br />
-                <br />
-                These rules make sure readers know if a blogger or publisher is sponsored or partnered with a company.
-                <br />
-                <br />
-                Readers need to know if the publisher makes money from sharing a link or product. Following FTC
-                guidelines, please assume this about links and posts on this site: Some or all links on
-                BeatMasterMind.com are affiliate links, which means we earn a small commission from sales.
+                you. The price is the same whether you use an affiliate link or not.
                 <h2>What are affiliate links?</h2>
                 When you click an affiliate link on BeatMasterMind.com and buy something, you buy it directly from the
-                seller (not from BeatMasterMind).
-                <br />
-                <br />
-                BeatMasterMind may get a small commission from Amazon or other companies for sending customers to their
-                websites. The price is the same for you whether you use an affiliate link or not.
-                <br />
+                seller (not from BeatMasterMind). We may get a small commission from Amazon or other companies for
+                sending customers to their websites. The price is the same for you whether you use an affiliate link or
+                not.
                 <br />
                 Clicking an affiliate link doesn’t change the price or anything else for you.{" "}
                 <h3>Amazon affiliate links</h3>
                 BeatMasterMind.com is part of the Amazon Services LLC Associates Program, an affiliate advertising
                 program designed to let sites earn fees by linking to Amazon.com and related sites.
-                <br />
                 <br />
                 If you click a product affiliate link and buy the product, we get a percentage of the sale or some other
                 type of compensation. Again, using these affiliate links doesn’t change the price for you. You won’t pay
@@ -46,6 +30,14 @@ export default function DisclaimerPage() {
                 <h2>Why trust our product recommendations?</h2>
                 By telling you about our use of affiliate links, we want to show our commitment to being open and honest
                 about the products we review.
+                <br />
+                <h2>FTC disclosure compliance rules</h2>
+                In 2015, the Federal Trade Commission set rules for Disclosure Compliance.
+                <br />
+                These rules make sure readers know if a blogger or publisher is sponsored or partnered with a company.
+                Readers need to know if the publisher makes money from sharing a link or product. Following FTC
+                guidelines, please assume this about links and posts on this site: Some or all links on
+                BeatMasterMind.com are affiliate links, which means we earn a small commission from sales.
                 <br />
                 <br />
                 <h1>AI-images</h1>
@@ -61,8 +53,10 @@ export default function DisclaimerPage() {
                 unless explicitly stated otherwise.
                 <br />
                 <br />
-                If you have any questions or concerns about the use of AI-generated images on this blog, feel free to
-                <Link href="/contact"> Contact</Link> us.
+                If you have any questions or concerns about the use of AI-generated images on this blog, feel free to{" "}
+                <Link href="/contact">Contact</Link> us.
+                <br />
+                <br />
               </div>
             </>
           </div>
