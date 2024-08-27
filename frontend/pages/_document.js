d356d696099a01c0d4b3cec2faeb63d228f05626
JoRuPeRu <johan.rutberg@iths.se>
2024-08-27 22:11:00 +0200

    Meta update

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -12,6 +12,29 @@ export default class MyDocument extends Document {
 
           <title>BeatMasterMind</title>
 
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+            rel="stylesheet"
+          />
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,});
+              `
+            }}
+          />
+
           {/* Start cookieyes banner */}
           <script
             id="cookieyes"
@@ -19,29 +42,6 @@ export default class MyDocument extends Document {
             src="https://cdn-cookieyes.com/client_data/41d17cc816402d90e0dd2d65/script.js"
           ></script>
           {/* End cookieyes banner */}
-
-          <scrip
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,});
-            `
-            }}
-          />
-
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
-            rel="stylesheet"
-          />
         </Head>
 
         <body>
