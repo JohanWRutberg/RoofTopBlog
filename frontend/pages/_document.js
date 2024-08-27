df9f6809de78d1043d75f5cc756a7ca624370d15
JoRuPeRu <johan.rutberg@iths.se>
2024-08-27 22:05:09 +0200

    Meta update

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -13,6 +13,12 @@ export default class MyDocument extends Document {
             src="https://cdn-cookieyes.com/client_data/41d17cc816402d90e0dd2d65/script.js"
           ></script>
           {/* End cookieyes banner */}
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="Your page description here" />
+
+          <title>BeatMasterMind</title>
+
           <scrip
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
@@ -28,11 +34,6 @@ export default class MyDocument extends Document {
             `
             }}
           />
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="description" content="Your page description here" />
-
-          <title>Your Page Title</title>
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
