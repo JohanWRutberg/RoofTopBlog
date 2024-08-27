90cfc9ea23dbae91dabbe0a7ba48bb88381ca163
JoRuPeRu <johan.rutberg@iths.se>
2024-08-27 22:07:12 +0200

    Meta update

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -6,6 +6,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="Your page description here" />
+
+          <title>BeatMasterMind</title>
+
           {/* Start cookieyes banner */}
           <script
             id="cookieyes"
@@ -13,11 +19,6 @@ export default class MyDocument extends Document {
             src="https://cdn-cookieyes.com/client_data/41d17cc816402d90e0dd2d65/script.js"
           ></script>
           {/* End cookieyes banner */}
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="description" content="Your page description here" />
-
-          <title>BeatMasterMind</title>
 
           <scrip
             async
