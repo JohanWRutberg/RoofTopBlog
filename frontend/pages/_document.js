5105bef3ab7c5f297fc98198d0dca098bf0911b5
JoRuPeRu <johan.rutberg@iths.se>
2024-08-28 11:53:13 +0200

    Error fix

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -21,7 +21,7 @@ export default function Document() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}');`}
+          })(window,document,'script','dataLayer','GTM-T26PQD5S');`}
         </Script>
         {/* End Google Tag Manager */}
 
@@ -47,9 +47,9 @@ export default function Document() {
           rel="stylesheet"
         />
 
-        {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} /> */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
-        <Script
+        <script
           dangerouslySetInnerHTML={{
             __html: `
                 window.dataLayer = window.dataLayer || [];
