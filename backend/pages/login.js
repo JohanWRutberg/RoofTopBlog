31e2e666417ea526ac0a77c205340536f605cf58
JoRuPeRu <johan.rutberg@iths.se>
2024-12-30 22:28:33 +0100

    Update login page logo image source

--- backend/pages/login.js
+++ backend/pages/login.js
@@ -33,7 +33,7 @@ export default function Login() {
     return (
       <>
         <div className="loginfront flex flex-center flex-col full-w">
-          <Image src="/img/Beat_Master.png" width={250} height={250} />
+          <Image src="/img/Logo/TopGearTent_Logo_Text.png" width={250} height={250} />
           <h1>Welcome Admin to the blog!</h1>
           <p>
             Visit the main website <a href="https://www.topgeartents.com">BMM</a>
