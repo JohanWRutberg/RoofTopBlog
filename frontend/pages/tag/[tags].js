b408699372522e1356f334a38d352e2eb50944c6
JoRuPeRu <johan.rutberg@iths.se>
2025-01-18 17:25:31 +0100

    Add SEO metadata to tag pages and refactor component structure for improved readability

--- frontend/pages/tag/[tags].js
+++ frontend/pages/tag/[tags].js
@@ -72,80 +72,100 @@ export default function CategoryPage({ initialData, tag }) {
   }
 
   return (
-    <div className="blogpage">
-      <div className="category_slug">
-        <div className="container">
-          <div className="category_title">
-            <div className="flex gap-1">
-              <h2>Tags: {loading ? <div>Loading... </div> : tag}</h2>
-              <span>{loading ? <div>0</div> : publishedblogs.filter((blog) => blog.tags).length}</span>
-            </div>
-          </div>
-          <div className="category_blogs mt-3">
-            {loading ? (
-              <div className="wh-100 flex flex-center mt-2 pb-5">
-                <div className="loader"></div>
+    <>
+      <Head>
+        <title>{tag ? `${tag} | TopGear Tents` : "TopGear Tents"}</title>
+        <meta name="keywords" content={tag || "Tags on TopGear Tents"} />
+        <meta property="og:title" content={tag || "Tags on TopGear Tents"} />
+        <meta
+          property="og:description"
+          content={blog.description ? blog.description.slice(0, 150) : "Blog post on TopGear Tents"}
+        />
+        <meta property="og:image" content={blog.image || "/default-image.png"} />
+        <meta property="og:url" content={`https://www.topgeartents.com${router.asPath}`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={tag || "Tags on TopGear Tents"} />
+        <meta
+          name="twitter:description"
+          content={blog.description ? blog.description.slice(0, 150) : "Blog post on TopGear Tents"}
+        />
+        <meta name="twitter:image" content={blog.image || "/default-image.png"} />
+      </Head>
+      <div className="blogpage">
+        <div className="category_slug">
+          <div className="container">
+            <div className="category_title">
+              <div className="flex gap-1">
+                <h2>Tags: {loading ? <div>Loading... </div> : tag}</h2>
+                <span>{loading ? <div>0</div> : publishedblogs.filter((blog) => blog.tags).length}</span>
               </div>
-            ) : (
-              publishedblogs.map((item) => {
-                const firstImageUrl = extractFirstImageUrl(item.description);
-                return (
-                  <div className="cate_blog" key={item._id}>
-                    <Link href={`/blog/${item.slug}`}>
-                      <Image src={firstImageUrl || "/img/noimage.jpg"} alt={item.title} height={830} width={1250} />
-                    </Link>
-
-                    <div className="bloginfo mt-2">
-                      <Link href={`/tag/${item.tags[0]}`}>
-                        <div className="blogtag">{item.tags[0]}</div>
-                      </Link>
+            </div>
+            <div className="category_blogs mt-3">
+              {loading ? (
+                <div className="wh-100 flex flex-center mt-2 pb-5">
+                  <div className="loader"></div>
+                </div>
+              ) : (
+                publishedblogs.map((item) => {
+                  const firstImageUrl = extractFirstImageUrl(item.description);
+                  return (
+                    <div className="cate_blog" key={item._id}>
                       <Link href={`/blog/${item.slug}`}>
-                        <h3>{item.title}</h3>
+                        <Image src={firstImageUrl || "/img/noimage.jpg"} alt={item.title} height={830} width={1250} />
                       </Link>
-                      <p>{getFirstWords(item.description)}</p>
-                      <div className="blogauthor flex gap-1">
-                        <div className="blogaimg">
-                          <Image src="/img/Logo/TopGearTent_Logo.png" width={50} height={50} alt="logo" />
-                        </div>
-                        <div className="flex flex-col flex-left gap-05">
-                          <h4>TopGear Tents</h4>
-                          <span>
-                            {new Date(item.createdAt).toLocaleDateString("en-US", {
-                              month: "long",
-                              day: "numeric",
-                              year: "numeric"
-                            })}
-                          </span>
+
+                      <div className="bloginfo mt-2">
+                        <Link href={`/tag/${item.tags[0]}`}>
+                          <div className="blogtag">{item.tags[0]}</div>
+                        </Link>
+                        <Link href={`/blog/${item.slug}`}>
+                          <h3>{item.title}</h3>
+                        </Link>
+                        <p>{getFirstWords(item.description)}</p>
+                        <div className="blogauthor flex gap-1">
+                          <div className="blogaimg">
+                            <Image src="/img/Logo/TopGearTent_Logo.png" width={50} height={50} alt="logo" />
+                          </div>
+                          <div className="flex flex-col flex-left gap-05">
+                            <h4>TopGear Tents</h4>
+                            <span>
+                              {new Date(item.createdAt).toLocaleDateString("en-US", {
+                                month: "long",
+                                day: "numeric",
+                                year: "numeric"
+                              })}
+                            </span>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })
-            )}
-          </div>
-          <div className="blogpagination">
-            <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
-              Previous
-            </button>
-            {pageNumbers
-              .slice(Math.max(currentPage - 3, 0), Math.min(currentPage + 2, pageNumbers.length))
-              .map((number) => (
-                <button
-                  key={number}
-                  onClick={() => paginate(number)}
-                  className={currentPage === number ? "active" : ""}
-                >
-                  {number}
-                </button>
-              ))}
-            <button onClick={() => paginate(currentPage + 1)} disabled={currentBlogs.length < perPage}>
-              Next
-            </button>
+                  );
+                })
+              )}
+            </div>
+            <div className="blogpagination">
+              <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+                Previous
+              </button>
+              {pageNumbers
+                .slice(Math.max(currentPage - 3, 0), Math.min(currentPage + 2, pageNumbers.length))
+                .map((number) => (
+                  <button
+                    key={number}
+                    onClick={() => paginate(number)}
+                    className={currentPage === number ? "active" : ""}
+                  >
+                    {number}
+                  </button>
+                ))}
+              <button onClick={() => paginate(currentPage + 1)} disabled={currentBlogs.length < perPage}>
+                Next
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
